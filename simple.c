7e1dc8ae77217e809a51cc471223395bb84ed0fe
Dennis Meme <dennismeme46@gmail.com>
2023-07-24 06:24:56 -0700

    simple code that passes betty checks

---
+++ simple.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - calculates two integers and prints sum.
+ * @m - first integer
+ * @n - second integer
+ * @sum - value of m and n when added.
+ * Return: 0 on success.
+ */
+
+int main(void)
+{
+        int m = 8;
+        int n = 10;
+        int sum = (m + n);
+
+        printf("sum is %d\n", sum);
+        return (0);
+}
