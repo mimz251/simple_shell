12f221f236e1575dad2949d8c2dee389dbcf7081
Maryannewambui <trimbleleewambui@gmail.com>
2023-07-25 06:59:34 -0700

    this file handles the command separator

---
+++ separator.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main() 
+{
+	char command[1000];
+
+	printf(": ");
+	fgets(command, sizeof(command), stdin);
+
+	char *token = strtok(command, ";");
+
+	while (token != NULL)
+	{
+		int start = 0;
+		while (token[start] == ' ' || token[start] == '\t')
+		{
+			start++;
+		}
+
+		int end = strlen(token) - 1;
+		while (end >= start && (token[end] == ' ' || token[end] == '\t' || token[end] == '\n'))
+		{
+			token[end] = '\0';
+			end--;
+		}
+
+		if (strlen(token) > 0)
+		{
+			int status = system(token);
+			if (status != 0)
+			{
+				printf("Command execution failed: %s\n", token);
+			}
+		}
+		token = strtok(NULL, ";");
+	}
+	return 0;
+}
