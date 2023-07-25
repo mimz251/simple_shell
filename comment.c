9c4ffcf4c0ff11876b7e8f53b6044b0c52f702b2
Maryannewambui <trimbleleewambui@gmail.com>
2023-07-25 06:50:58 -0700

    this file handles comments #

---
+++ comment.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define max_command_length 200
+
+int main() 
+{
+	char command [max_command_length];
+	while (1) 
+	{
+		printf("$ ");
+		fgets(command, sizeof(command), stdin);
+
+		command[strcspn(command, "\n")] = '\0';/*removes trailing space*/
+
+		if (strcmp(command, "exit") == 0) 
+		{
+			break; /*Exit the shell*/
+		} 
+		else 
+		{
+			char* comment_pos = strchr(command, '#');
+			if (comment_pos != NULL) 
+			{
+				*comment_pos = '\0';
+			}
+
+			int status = system(command);
+
+			if (status != 0) 
+			{
+				printf("Command execution failed.\n");
+			}
+		}
+	}
+
+	return (0);
+}
