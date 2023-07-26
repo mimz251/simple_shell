5eba83f22d25af6069f4f7bab9b45f214802763b
Maryannewambui <trimbleleewambui@gmail.com>
2023-07-25 22:16:57 -0700

    this file handles the path of files and directories

---
+++ path.c
@@ -0,0 +1,41 @@
+#include <dirent.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	/*accessing the directory*/
+	DIR *directory;
+
+	/*store pointer*/
+	struct dirent *entry;
+
+	directory = opendir("."); /*open directory*/
+
+	if (directory == NULL)
+	{
+		printf("Can't open directory.\n");
+		return (1);
+	}
+
+	while ((entry = readdir(directory)) != NULL)
+	{
+		if (entry->d_type == DT_REG)
+		{
+			printf("File: %s\n", entry->d_name);
+		}
+
+		else if (entry->d_type == DT_DIR)
+		{
+			printf(" dir: %s\n", entry->d_name);
+		}
+	}
+	if (closedir(directory) == -1)
+	{
+		printf("Error closing directory.\n");
+		return (1);
+	}
+
+	return (0);
+}
