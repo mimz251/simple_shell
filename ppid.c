25e3749b75d2b95242bfa85c0a71632485dfc5dc
Maryannewambui <trimbleleewambui@gmail.com>
2023-07-26 00:17:11 -0700

    This is the process id

--- ppid.c
+++ ppid.c
@@ -2,9 +2,11 @@
 #include <unistd.h>
 
 /**
- * main - entry point
+ * main - gives the process id of child and parent
  * pid - process id
  * ppid - parent process id
+ * Return: 0 success
+ * else 1 unsuccessful
  */
 int main(void)
 {
