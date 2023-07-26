57bfbefe2f60a4a4483cc3ef092d6256af1d67c2
Maryannewambui <trimbleleewambui@gmail.com>
2023-07-25 22:38:34 -0700

    this file handles execution process

---
+++ execve.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(void)
+{
+	pid_t pid;
+	char *av[] = {"/bin/ls", "-l", NULL};
+
+	pid = fork();
+
+	if (pid == -1)
+	{
+		return (-1);
+	}
+	if (pid == 0)
+	{
+		int val = execve(av[0], av, NULL);
+		if (val ==-1)
+			perror("Error");
+	}
+	else
+	{
+		wait(NULL);
+		printf("execve successful\n");
+	}
+	return (0);
+}
