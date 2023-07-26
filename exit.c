cbf68aa7e0feb457eb28b3f0e64f07186bd67806
Maryannewambui <trimbleleewambui@gmail.com>
2023-07-26 00:33:09 -0700

    this file handles argument for the built-in exit

--- exit.c
+++ exit.c
@@ -4,22 +4,28 @@
 
 #define max_command_length 200
 
-int main() 
+/**
+ * main - allow the function that handles the exit status
+ * Return: 0 success
+ */
+int main(void)
 {
 	char command[max_command_length];
 
-	while (1) 
+	while (1)
 	{
 		printf("$ ");
 		fgets(command, sizeof(command), stdin);
 
-		command[strcspn(command, "\n")] = '\0';/* Remove trailing newline from the command*/
+		command[strcspn(command, "\n")] = '\0';
+		/* Remove trailing newline from the command*/
 
 		if (strncmp(command, "exit", 4) == 0)
 		{
 			/*Parse the exit status*/
 			int status = 0;
-			char* arg = strtok(command, " ");
+			char *arg = strtok(command, " ");
+
 			arg = strtok(NULL, " ");
 
 			if (arg != NULL)
@@ -27,10 +33,11 @@ int main()
 				status = atoi(arg);
 			}
 			exit(status);
-		} 
+		}
 		else
 		{
 			int status = system(command);
+
 			if (status != 0)
 			{
 				printf("Command execution failed.\n");
