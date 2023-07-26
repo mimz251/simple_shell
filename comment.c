60a7b538f8e3c37d3af62b9205077e190fa2505c
Maryannewambui <trimbleleewambui@gmail.com>
2023-07-26 00:50:47 -0700

    this file handles the comment #

--- comment.c
+++ comment.c
@@ -3,31 +3,37 @@
 #include <string.h>
 #define max_command_length 200
 
-int main() 
+/**
+ * main - this function allows a comment in the shell
+ * Return: 0
+ */
+int main(void)
 {
-	char command [max_command_length];
-	while (1) 
+	char command[max_command_length];
+
+	while (1)
 	{
 		printf("$ ");
 		fgets(command, sizeof(command), stdin);
 
 		command[strcspn(command, "\n")] = '\0';/*removes trailing space*/
 
-		if (strcmp(command, "exit") == 0) 
+		if (strcmp(command, "exit") == 0)
 		{
-			break; /*Exit the shell*/
-		} 
-		else 
+			break;/*Exit the shell*/
+		}
+		else
 		{
-			char* comment_pos = strchr(command, '#');
-			if (comment_pos != NULL) 
+			char *comment_pos = strchr(command, '#');
+
+			if (comment_pos != NULL)
 			{
 				*comment_pos = '\0';
 			}
 
 			int status = system(command);
 
-			if (status != 0) 
+			if (status != 0)
 			{
 				printf("Command execution failed.\n");
 			}
