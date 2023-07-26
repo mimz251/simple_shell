47238bc16f89d766ceba246e630530a276498176
Maryannewambui <trimbleleewambui@gmail.com>
2023-07-26 01:12:15 -0700

    this file handles the command separator

--- separator.c
+++ separator.c
@@ -3,7 +3,11 @@
 #include <string.h>
 #include <unistd.h>
 
-int main() 
+/**
+ * main - function that handles the separator used in shell
+ * Return: 0 success
+ */
+int main(void)
 {
 	char command[1000];
 
@@ -15,12 +19,14 @@ int main()
 	while (token != NULL)
 	{
 		int start = 0;
+
 		while (token[start] == ' ' || token[start] == '\t')
 		{
 			start++;
 		}
 
 		int end = strlen(token) - 1;
+
 		while (end >= start && (token[end] == ' ' || token[end] == '\t' || token[end] == '\n'))
 		{
 			token[end] = '\0';
@@ -30,6 +36,7 @@ int main()
 		if (strlen(token) > 0)
 		{
 			int status = system(token);
+
 			if (status != 0)
 			{
 				printf("Command execution failed: %s\n", token);
@@ -37,5 +44,5 @@ int main()
 		}
 		token = strtok(NULL, ";");
 	}
-	return 0;
+	return (0);
 }
