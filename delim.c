8f9c1c79694e7c6e9db348b19623359253aaf785
Maryannewambui <trimbleleewambui@gmail.com>
2023-07-26 01:20:16 -0700

    delimeter using blank space

--- delim.c
+++ delim.c
@@ -2,23 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main() {
-    char* input = NULL;
-    size_t bufsize = 0;
+/**
+ * main - function that allows the separation os small parts of a program
+ * Return: 0 success
+ */
+int main(void)
+{
+	char *input = NULL;
+	size_t bufsize = 0;
 
-    printf("Enter a sentence: ");
-    getline(&input, &bufsize, stdin);
+	printf("Enter a sentence: ");
+	getline(&input, &bufsize, stdin);
 
-    input[strcspn(input, "\n")] = '\0';
+	input[strcspn(input, "\n")] = '\0';
 
-    char* token = strtok(input, " ");
+	char *token = strtok(input, " ");
 
-    while (token != NULL) {
-        printf("%s\n", token);
-        token = strtok(NULL, " ");
-    }
+	while (token != NULL)
+	{
+		printf("%s\n", token);
+		token = strtok(NULL, " ");
+	}
 
-    free(input);
+	free(input);
 
-    return 0;
+	return (0);
 }
