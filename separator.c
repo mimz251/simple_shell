25aba40c31a9d410f9f445c52456dca98eee04ca
Dennis Meme <dennismeme46@gmail.com>
2023-07-27 11:20:37 -0700

    separates different commands given

--- separator.c
+++ separator.c
@@ -1,47 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
-#define max_command_length 200
+#define MAX_COMMAND_LENGTH 200
+
 /**
- * main - function that handles the separator used in shell
- * Return: 0 success
+ * execute_command: Function that executes the given command.
+ * @command: The command to be executed.
+ * reads the input using getline() in the input from stdin and the output.
+ * main - shell program that runs shell commands.
  */
-
 void execute_command(const char *command)
 {
 	int status = system(command);
 
-	if (status != 0)
+	if (status == -1)
 	{
 		printf("Command execution failed.\n");
+	}
+	else
+	{
+		if (WIFEXITED(status))
+		{
+			int exit_status = WEXITSTATUS(status);
+
+			printf("Command exited with status: %d\n", exit_status);
+		}
+		else
+		{
+			printf("Command did not exit normally.\n");
+		}
 	}
 }
 
 int main(void)
 {
-	char *command = NULL;
-	size_t bufsize = 0;
+	size_t n = 0;
+	ssize_t read;
+	char *buff = NULL;
 
 	while (1)
 	{
-		printf("$ ");
-		if (getline(&command, &bufsize, stdin) == -1)
+		printf("collabo$ ");
+
+		read = getline(&buff, &n, stdin);
+		if (read != -1)
 		{
+			buff[strcspn(buff, "\n")] = '\0';
+
+			char *token = strtok(buff, ";");
+
+			while (token != NULL)
+			{
+				execute_command(token);
+				token = strtok(NULL, ";");
+			}
+		}
+		if (read == -1)
+		{
+			perror("Error");
 			break;
 		}
-		command[strcspn(command, "\n")] = '\0';
 
-		char *token = strtok(command, ";");
-
-		while (token != NULL)
-		{
-			execute_command(token);
-
-			token = strtok(NULL, ";");
-		}
+		free(buff);
+		buff = NULL;
 	}
-	free(command);
+
 	return (0);
 }
+
